cec60a752d6cb0d0ebf77d2569492f5365e70c49
Cain <cainmartinez2@hotmail.com>
2024-04-10 14:16:37 +0200

    Update login module: Refactor controller_login.php and register.html for improved security and user experience

---
+++ model/middleware_auth.php
@@ -0,0 +1,35 @@
+<?php
+    include('C:\xampp\htdocs\coches_net\model\JWT.php');
+
+    function decode_token($token){
+        $jwt = parse_ini_file('C:\xampp\htdocs\coches_net\model\data.ini', true);
+        $secret = $jwt['jwt_credentials']['secret'];
+    
+        $JWT = new JWT;
+        $token_dec = $JWT->decode($token, $secret);
+        $rt_token = json_decode($token_dec, TRUE);
+        return $rt_token;
+    }
+    
+    function create_token($username){
+        $jwt = parse_ini_file('C:\xampp\htdocs\coches_net\model\data.ini', true);
+        $header = $jwt['jwt_credentials']['header'];
+        $secret = $jwt['jwt_credentials']['secret'];
+        $payload = '{"iat":"' . time() . '","exp":"' . time() + (14400) . '","username":"' . $username . '"}';
+    
+        $JWT = new JWT;
+        $token = $JWT->encode($header, $payload, $secret);
+        return $token;
+    }
+
+    function create_token_refresh($username){
+        $jwt = parse_ini_file('C:\xampp\htdocs\coches_net\model\data.ini', true);
+        $header = $jwt['jwt_credentials']['header'];
+        $secret = $jwt['jwt_credentials']['secret'];
+        $payload = '{"iat":"' . time() . '","exp":"' . time() + (30) . '","username":"' . $username . '"}'; // EL tiempo para 1 hora es 600
+    
+        $JWT = new JWT;
+        $token = $JWT->encode($header, $payload, $secret);
+        return $token;
+    }
+?>
--- module/login/controller/controller_login.php
+++ module/login/controller/controller_login.php
@@ -14,7 +14,7 @@
     
             echo json_encode($rdo); 
     
-            break;
+        break;
         
         case 'login':
             // echo json_encode($_POST['passwordLogin']);
@@ -29,17 +29,21 @@
                 echo json_encode("error_username");
                 exit;
             } else if(password_verify($_POST['passwordLogin'], $rdo)) {
+                $token = create_token_refresh($_POST['usernameLogin']);
                 $_SESSION['username'] = $_POST['usernameLogin'];
-                $_SESSION['time_out'] = time();
-                echo json_encode($output);
-                exit;
+                $_SESSION['tiempo'] = time();
+                $token_large = create_token($_POST['usernameLogin']);
+                $output = [
+                    'token_large' => $token_large,
+                    'token_refresh' => $token,
+                ];
             } else {
                 echo json_encode("error_password");
                 exit;
             }
     
             echo json_encode($rdo);
-            break;
+        break;
             
         case 'logout':
             unset($_SESSION['username']);
@@ -47,6 +51,79 @@
             session_destroy();
     
             echo json_encode('Done');
-            break;
+        break;
+        
+        case 'dataUser':
+            $json = decode_token($_POST['token']);
+            $daoLogin = new DAOLogin();
+            $rdo = $daoLogin -> selectDataUser($json['username']);
+            echo json_encode($rdo);
+            exit;
+        break;
+    
+        case 'controlUser':
+            $parseToken = decode_token($_POST['token']);
+    
+            if($parseToken['exp'] < time()){
+                echo json_encode("wrongUser");
+                exit;
+            }
+    
+            if(isset($_SESSION['username']) && ($_SESSION['username']) == $parseToken['username']){
+                echo json_encode("correctUser");
+                exit;
+            } else {
+                echo json_encode("wrongUser");
+                exit;
+            }
+        break;
+    
+        case 'controlActivity':
+            if (!isset($_SESSION["tiempo"])) {
+                echo json_encode("inactivo");
+                exit();
+            } else {
+                if ((time() - $_SESSION["tiempo"]) >= 1800) { //1800s=30min
+                    echo json_encode("inactivo");
+                    exit();
+                } else {
+                    echo json_encode("activo");
+                    exit();
+                }
+            }
+        break;
+    
+        case 'checkExpirationTokenRefresh':
+            $tokenRefreshDec = decode_token($_POST['token_refresh']);
+            $tokenLargeDec = decode_token($_POST['token_large']);
+    
+            
+            // Comprobamos si Token Refresh ha caducado
+            if ($tokenRefreshDec['exp'] < time()) {
+                if ($tokenLargeDec['exp'] > time()) {
+                    // Devolvemos que hay que generar token nuevo
+                    echo json_encode("ExpiredJWTRefresh");
+                } else {
+                    // Devolvemos que han caducado los dos y hay que hacer logout
+                    echo json_encode("ExpiredJWTToken");
+                }
+            } else {
+                echo json_encode("NotExpiredJWTRefresh");
+            }
+        break;
+        
+        case 'changeTokenRefres':
+            // Comprobamos que el usuario del token es el mismo que el de la session
+            $parseToken = decode_token($_POST['token']);
+            if(isset($_SESSION['username']) && ($_SESSION['username']) == $parseToken['username']){
+                $token = create_token_refresh($_SESSION['username']);
+                echo json_encode($token);
+                exit;
+            } else {
+                echo json_encode("wrongUser");
+                exit;
+            }
+    
+        break;
     }
 ?>
