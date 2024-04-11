53c56adc708d91b0538bebd37a5a26bb92c038f4
Cain <cainmartinez2@hotmail.com>
2024-04-11 20:19:53 +0200

    Refactor login form for improved user experience and validation.

--- module/login/model/controller_login.js
+++ module/login/model/controller_login.js
@@ -15,11 +15,11 @@ function validate_login() {
     var error = false;
 
     if($('#usernameLogin').val().length === 0) {
-        $('#errorUsernameLogin').html('<br>Introduce tu nombre de usuario');
+        $('#errorUsernameLogin').html('<br>Enter your username.');
         error = true;
     } else {
         if($('#usernameLogin').val().length < 8) {
-            $('#errorUsernameLogin').html('<br>El nombre de usuario tiene como mínimo 8 caracteres');
+            $('#errorUsernameLogin').html('<br>The user name has at least 8 characters.');
             error = true;
         } else {
             $('#errorUsernameLogin').html('');
@@ -27,7 +27,7 @@ function validate_login() {
     }
 
     if($('#passwordLogin').val().length === 0) {
-        $('#errorPasswordLogin').html('<br>Introduce la contraseña');
+        $('#errorPasswordLogin').html('<br>Enter the password.');
         error = true;
     } else {
             $('#errorPasswordLogin').html('');
@@ -41,14 +41,14 @@ function login() {
     function promiseLogin() {
         let data = $('#loginForm').serialize();
         // console.log(data);
-        ajaxPromise("module/login/ctrl/ctrl_login.php?op=login", 'POST', 'JSON', data)
+        ajaxPromise('POST', 'JSON','module/login/ctrl/ctrl_login.php?op=login',  data)
         .then(function (data) {
             // console.log(data['token_large']);
             // console.log(data['token_refresh']);
             let error = false;
-            data == "error_username" ? ( $('#errorUsernameLogin').html('<br>El nombre de usuario introducido no existe'), error = true) : undefined;
-            data == "error_password" ? ( $('#errorPasswordLogin').html('<br>Datos erroneos, revisa el nombre de usuario y la contraseña'), error = true) : undefined;
-            error == false ? ( localStorage.setItem("token", data['token_large']), localStorage.setItem("token_refresh", data['token_refresh']), toastr.success("Bienvenido de nuevo"), setTimeout(' window.location.href = "index.php?page=ctrl_shop&op=list"; ', 1000) ) : undefined;
+            data == "error_username" ? ( $('#errorUsernameLogin').html('<br>The user name entered does not exist.'), error = true) : undefined;
+            data == "error_password" ? ( $('#errorPasswordLogin').html('<br>Erroneous data, check username and password.'), error = true) : undefined;
+            error == false ? ( localStorage.setItem("token", data['token_large']), localStorage.setItem("token_refresh", data['token_refresh']), toastr.success("Welcome back"), setTimeout(' window.location.href = "index.php?page=shop"; ', 1000) ) : undefined;
         }).catch(function() {
             console.log("error ajaxForSearch Login");
         });
